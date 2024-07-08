f53186ddf371aefd0f8504e4898a742d7bf8d9a1
derpen <rizqyassabil@hotmail.com>
2024-07-09 02:05:08 +0700

    feat: Footer kind of done

--- unsri-students-database/unsri-doxxer-frontend/src/app/components/footer.tsx
+++ unsri-students-database/unsri-doxxer-frontend/src/app/components/footer.tsx
@@ -1,7 +1,27 @@
 export default function Footer(){
   return (
-    <h2 className="min-h-screen">
-      Hello, this is footer content :)
-    </h2>
+    <footer className="min-h-screen bg-gray-800 text-white content-center flex p-5 justify-between">
+      <div className="p-2">
+        <p>
+          Unsri-Doxxer-5000, the best way to dox some dudes
+        </p>
+        <p>
+          Copyleft, 2024 @derpen
+        </p>
+      </div>
+      <div className="flex">
+        <p className="p-2">
+          V 1.0
+        </p>
+        <p className="p-2">
+          License: GPL 3.0
+        </p>
+        {/* TODO: This should be link */}
+        <p className="p-2">
+          Github
+        </p>
+      </div>
+
+    </footer>
   )
 }
